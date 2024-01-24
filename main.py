e179e6bb580df5de577be6141690856f66465e29
stephen19k <liaostphn@gmail.com>
2024-01-24 12:54:44 -0800

    hello world

---
+++ main.py
@@ -0,0 +1,11 @@
+import cassiopeia as cass
+from cassiopeia import Summoner
+
+"""
+Get match history with date and time
+Heatmap with x-axis: day of week | y-axis: time of day | intensity: # of matches
+"""
+print("hai world")
+
+platapierule = Summoner(name="platapierule", region="NA")
+print(platapierule)
