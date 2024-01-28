1f9fd615ef56032a30da3b059bb1769caeb51188
stephen19k <liaostphn@gmail.com>
2024-01-28 12:38:11 -0800

    pivot dataframe and add heatmap

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from itertools import count
 import cassiopeia as cass
 from cassiopeia.cassiopeia import get_champion
 import pandas as pd
+import seaborn
+import matplotlib.pyplot as plt
 from cassiopeia import Summoner, Patch, Champions, Match, MatchHistory, Lane
 from cassiopeia.data import Continent
 
 """
 1. Get match history with date and time
+    - can use DataFrame.from_dict()
+    - cols: match_id, datetime, result(w/l)
+        - additional cols that can derived: day_of_week, hour_of_day
 2. Heatmap with x-axis: day of week | y-axis: time of day | intensity: # of matches
 
 dataframe schema
@@ -17,35 +22,47 @@ dataframe schema
     .
 """
 def main():
-    API_KEY = "RGAPI-42f1303d-57b2-47ab-ad6a-e33923f1b699"
+    API_KEY = "RGAPI-c9474098-f1de-497d-88c1-1a03efc96e05"
     NA = "NA"
     cass.set_riot_api_key(API_KEY)
     # all_champions = Champions(region="NA")
     # teemo = all_champions["Teemo"]
     platapierule = Summoner(name="platapierule", region=NA)
-    mh = cass.get_match_history(puuid=platapierule.puuid, count=5, continent=Continent.americas
+    mh = cass.get_match_history(puuid=platapierule.puuid, count=100, continent=Continent.americas
 )
     # print(type(mh))
     # print(len(mh))
     # print(mh.start_time)
+    match_history_dict = {}
+    match_history_dict["match_id"] = []
+    match_history_dict["start"] = []
+    match_history_dict["result"] = []
+    match_history_dict["day"] = []
+    match_history_dict["hour"] = []
     recent = mh
+
     for match in recent:
-        print(match.start)
-        for p in match.participants:
-            if p.summoner.name == "platapierule":
-                print(p.champion, p.lane)
-                # print(get_champion(key=p.champion.id, region=NA))
+        print(match.start, match.start.humanize())
+        champion_played = match.participants[platapierule].champion
+        print(champion_played, cass.get_champion(key=champion_played, region=NA))
+        print("WINNNN???" + str(match.participants[platapierule].team.win))
+        match_history_dict["match_id"].append(match.id)
+        arrow = match.start.to('US/Pacific')
+        match_history_dict["start"].append(arrow)
+        match_history_dict["day"].append(arrow.format('dddd'))
+        match_history_dict["hour"].append(arrow.format('H'))
+        match_history_dict["result"].append(str(match.participants[platapierule].team.win))
 
-    # first_match = mh[0]
-    # get match date
-    # print(
-    #     first_match.start,
-    #     first_match.duration,
-    #     first_match.blue_team.bans,
-    #     first_match.continent,
-    #     first_match.blue_team.win)
-    # print(first_match)
 
+    print(match_history_dict)
+    match_data = pd.DataFrame.from_dict(match_history_dict) #.pivot(index="time_of_day", columns="day", values="count")
+    pivot_match_data = match_data.pivot_table(index="hour", columns="day", values="match_id", aggfunc="count")
+    # match_data['month'] = match_data.start.month
+    print(match_data)
+    print(pivot_match_data)
+
+    seaborn.heatmap(pivot_match_data, annot=True)
+    plt.show()
 
 
 if __name__ == "__main__":
